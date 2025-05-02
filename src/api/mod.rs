51a5c6549b4fcfcd8c8ec89d717c7d4f9acc274e
Maxime Demolin <maxime_dev@demol.in>
2025-05-02 21:21:32 +0200

    Update mockito
    This patch is updating [mockito] crate to its latest version.
    
    [1]: https://crates.io/crates/mockito

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -12,18 +12,37 @@ const BASE_URL: &str = "https://app.api.surehub.io";
 use crate::utils::authentication::bearer_token;
 
 /// Make an authenticated HTTP GET to the API.
+#[cfg(not(test))]
 async fn get_resources(path: &str) -> Response {
-    #[cfg(not(test))]
     let bearer_token = bearer_token().await;
-
-    #[cfg(test)]
-    let bearer_token = "some_token";
-
-    #[cfg(not(test))]
     let url = format!("{}{}", BASE_URL, path);
 
-    #[cfg(test)]
-    let url = format!("{}{}", &mockito::server_url(), path);
+    let client = reqwest::Client::builder()
+        .user_agent("surepet-cli")
+        .build()
+        .unwrap();
+    let response = client
+        .get(url)
+        .header("AUTHORIZATION", format!("Bearer {}", bearer_token))
+        .send()
+        .await
+        .unwrap();
+
+    match response.status() {
+        reqwest::StatusCode::OK => return response,
+        reqwest::StatusCode::UNAUTHORIZED => {
+            panic!("Need to grab a new token");
+        }
+        _ => {
+            panic!("Uh oh! Something unexpected happened.");
+        }
+    }
+}
+
+#[cfg(test)]
+async fn get_resources(path: &str) -> Response {
+    let bearer_token = "some_token";
+    let url = format!("{}{}", std::env::var("MOCKITO_URL").unwrap(), path);
 
     let client = reqwest::Client::builder()
         .user_agent("surepet-cli")
